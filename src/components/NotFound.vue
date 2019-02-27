361e21c545a91bb3b7522a3d3a338d2b6bce519c
eylee <hbzcir@naver.com>
2019-02-27 19:21:30 +0900

    Add files via upload

---
+++ src/components/NotFound.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="container">
+  <h2>Err</h2>
+  404
+</div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            todos: [
+                {
+                    name: '청소'
+                },
+                {
+                    name: '공부'
+                },
+                {
+                    name: '잠자기'
+                },
+                {
+                    name: '먹기'
+                }
+            ]
+        }
+    },
+    mounted () {
+        console.log('component mounted')
+    }
+}
+</script>
